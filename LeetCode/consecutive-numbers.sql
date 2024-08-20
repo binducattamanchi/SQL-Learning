72e908d2830199990b8c0764a3231da5e9c7aee2
binducattamanchi <32897934+binducattamanchi@users.noreply.github.com>
2024-08-20 14:00:04 -0500

    Create consecutive-numbers.sql

---
+++ LeetCode/consecutive-numbers.sql
@@ -0,0 +1,56 @@
+########################################################################################
+########## QUESTION  ##########
+########################################################################################
+/*
+  Table: Logs
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+ 
+
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+*/
+########################################################################################
+########## ANSWER  ##########
+########################################################################################
+select A.Num as ConsecutiveNums
+from Logs A, Logs B, Logs C
+where A.Num=B.num and B.num=C.num
+and A.Id=B.Id+1
+and A.Id=C.Id+2
+group by 1;
