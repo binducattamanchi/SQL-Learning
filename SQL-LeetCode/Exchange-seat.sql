26ba4006a75a0c7859ba6484ec90dea47f9d3cbb
binducattamanchi <32897934+binducattamanchi@users.noreply.github.com>
2024-09-24 00:44:23 -0500

    Update Exchange-seat.sql
    Added Bindu Cattamanchi

--- SQL-LeetCode/Exchange-seat.sql
+++ SQL-LeetCode/Exchange-seat.sql
@@ -20,21 +20,21 @@ Example 1: Input: Seat table:
 +----+---------+
 | id | student |
 +----+---------+
-| 1  | Abbot   |
-| 2  | Doris   |
-| 3  | Emerson |
-| 4  | Green   |
-| 5  | Jeames  |
+| 1  | Surya   |
+| 2  | Abhay   |
+| 3  | Ram |
+| 4  | Cattamanchi   |
+| 5  | Bindu  |
 +----+---------+
 Output: 
 +----+---------+
 | id | student |
 +----+---------+
-| 1  | Doris   |
-| 2  | Abbot   |
-| 3  | Green   |
-| 4  | Emerson |
-| 5  | Jeames  |
+| 1  | Surya   |
+| 2  | Abhay   |
+| 3  | Ram   |
+| 4  | Cattamanchi |
+| 5  | Bindu  |
 +----+---------+
 Explanation: 
 Note that if the number of students is odd, there is no need to change the last one's seat.*/
